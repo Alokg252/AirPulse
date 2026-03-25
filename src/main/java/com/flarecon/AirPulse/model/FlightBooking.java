c87bfe7dc06ea2baf98c3366b0347ff25042e7c3
Alokg252 <alokg252@gmail.com>
2026-03-25 09:38:28 +0530

    added flight booking

---
+++ src/main/java/com/flarecon/AirPulse/model/FlightBooking.java
@@ -0,0 +1,23 @@
+package com.flarecon.AirPulse.model;
+
+import com.flarecon.AirPulse.model.core.AbstractAuditingEntity;
+import com.flarecon.AirPulse.model.user.User;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "flight_booking")
+public final class FlightBooking extends AbstractAuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "flight_id")
+    private Flight flight;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
