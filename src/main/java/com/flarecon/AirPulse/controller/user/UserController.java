ee50dddd6195f775903c76da931d58763cef3857
Alokg252 <alokg252@gmail.com>
2026-03-25 11:41:55 +0530

    fixes

--- src/main/java/com/flarecon/AirPulse/controller/user/UserController.java
+++ src/main/java/com/flarecon/AirPulse/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.flarecon.AirPulse.model.user.User;
 import com.flarecon.AirPulse.model.user.UserGender;
 import com.flarecon.AirPulse.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ public class UserController {
     private final UserRepository userRepository;
 
     @PostMapping("/create")
-    public ResponseEntity<UserDto> createUser(@RequestBody Map<String, String> request) {
+    public ResponseEntity<User> createUser(@RequestBody Map<String, String> request) {
         User user = new User();
         user.setEmail(request.get("email"));
         user.setUsername(request.get("username"));
@@ -30,12 +31,12 @@ public class UserController {
         user.setPlainPassword(request.get("password"));
         user.setUuid(UUID.randomUUID().toString());
 
-        return ResponseEntity.ok(new UserDto(userRepository.saveAndFlush(user)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.saveAndFlush(user));
     }
 
     @GetMapping("/username/{username}")
-    public ResponseEntity<UserDto> findUserByUsername(@PathVariable String username) {
-        return ResponseEntity.ok(new UserDto(userRepository.findByUsername(username).orElseThrow()));
+    public ResponseEntity<User> findUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(userRepository.findByUsername(username).orElseThrow());
     }
 
 }
